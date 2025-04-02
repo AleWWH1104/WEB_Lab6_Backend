910e8688a5402f75d58c21c43d12bed82cade81b
AleWWH1104 <irisale.ayala@gmail.com>
2025-04-01 21:43:43 -0600

    bd y docker

---
+++ database/initDB.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE seriesdb;
+
+\c seriesdb;
+
+CREATE TABLE IF NOT EXISTS series (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) UNIQUE NOT NULL,
+    status TEXT CHECK (status IN ('Plan to Watch', 'Watching', 'Dropped', 'Completed')) NOT NULL,
+    last_episode_watched INT DEFAULT 0,
+    total_episodes INT,
+    ranking INT
+);
